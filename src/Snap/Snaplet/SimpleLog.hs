f82a1a50ee23cc1ec7f8be3588c48c91107e6e67
Alexandr Ruchkin <voidex@live.com>
2012-08-01 10:45:44 -0700

    New simple-log with politics

--- src/Snap/Snaplet/SimpleLog.hs
+++ src/Snap/Snaplet/SimpleLog.hs
@@ -20,9 +20,9 @@ import System.Log
 data SimpleLog = SimpleLog {
   simpleLog :: Log }
 
-simpleLogInit :: [IO Logger] -> SnapletInit b SimpleLog
-simpleLogInit ls = makeSnaplet "log" "Simple log" Nothing $ do
-  l <- liftIO $ newLog ls
+simpleLogInit :: Politics -> Rules -> [IO Logger] -> SnapletInit b SimpleLog
+simpleLogInit p rs ls = makeSnaplet "log" "Simple log" Nothing $ do
+  l <- liftIO $ newLog p rs ls
   return $ SimpleLog l
 
 instance MonadLog (Handler b SimpleLog) where
